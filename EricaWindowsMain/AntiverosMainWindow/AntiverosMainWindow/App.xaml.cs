8a0d82e4a682fec8001c1321cfc5309afbdb1429
Heath <143572562+AntiverosErica@users.noreply.github.com>
2025-10-22 14:49:43 -0930

    Add files via upload

---
+++ EricaWindowsMain/AntiverosMainWindow/AntiverosMainWindow/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AntiverosMainWindow
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
